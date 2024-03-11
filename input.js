b384ffe7576a4fa4bfec814df3ddcd37533d5e32
AdiaGinyard <143430888+AdiaGinyard@users.noreply.github.com>
2024-03-11 16:05:49 +0000

    Final

--- input.js
+++ input.js
@@ -1,7 +1,37 @@
+let c1o1 = document.querySelector('.c1o1') ;
+let c1o2 = document.querySelector('.c1o2') ;
+let c2o1 = document.querySelector('.c2o1') ;
+let c2o2 = document.querySelector('.c2o2') ;
+let c3o1 = document.querySelector('.c3o1') ;
+let c3o2 = document.querySelector('.c3o2') ;
 let button = document.querySelector("button");
+let q1 
+let q2
+let q3
 
 
-button.addEventListener("click", function(){
+c1o1.addEventListener("click", function() {
+  q1=1;
+   });
 
+ c1o2.addEventListener("click", function() {
+  q1=2;
+  });
 
-});
+ c2o1.addEventListener("click", function() {
+   q2=1;
+   });
+
+ c2o2.addEventListener("click", function() {
+   q2=2;
+   });
+
+  c3o1.addEventListener("click", function() {
+  q3=1;
+  });
+
+  c3o2.addEventListener("click", function() {
+  q3=2;
+  });
+                 
+   
